cf3c4b394baaa203a0d9789e42d0f4c5358efde8
Ricardo Lopes <ricardoxlopes@hotmail.com>
2018-03-29 14:42:36 +0100

    script logs

--- present.sh
+++ present.sh
@@ -2,12 +2,16 @@
 
 path='hfc-key-store'
 ca1_url='http://localhost:7054'
+ca3_url='http://localhost:9054'
 ca4_url='http://localhost:10054'
 
 ca1_name='ca-org1'
 ca4_name='ca-Pl'
+ca3_name='ca-org3'
+
 
 mspid1='Org1MSP'
+mspid3='Org3MSP'
 mspid4='Pl1MSP'
 
 user='user'
@@ -25,17 +29,51 @@ args2='["b"]'
 
 node enrollAdmin.js $path $ca1_url $ca1_name $mspid1
 node registerUser.js $path $ca1_url 'user' $mspid1
+echo 
+echo "################################"
+echo "QUERY ORG1"
+echo "################################"
+echo 
 val=$(node query.js $path 'mychannel1' $peer4 'user' 'mycc' 'query' $args1)
 
-echo "##### BLOCK QUERY $val #########"
+echo "BLOCK QUERY $val"
+
 
 args3='["a","b","'$val'"]'
 
 node enrollAdmin.js $path'1' $ca4_url $ca4_name $mspid4
 node registerUser.js $path'1' $ca4_url 'user1' $mspid4
+echo 
+echo "################################"
+echo "TRANSACT TO PRIVATE LEDGER"
+echo "################################"
+echo 
 node invoke.js $path'1' 'mychannel4' $peer4 $orderer 'user1' 'mycc' 'invoke' $args3 $event_peer
-node query.js $path'1' 'mychannel4' $peer4 'user1' 'mycc' 'query' $args2
+echo 
+echo "################################"
+echo "QUERY PRIVATE LEDGER"
+echo "################################"
+echo 
+val=$(node query.js $path'1' 'mychannel4' $peer4 'user1' 'mycc' 'query' $args2)
 
+echo "BLOCK QUERY $val"
+
+args4='["b","a","'$val'"]'
+
+node enrollAdmin.js $path'2' $ca3_url $ca3_name $mspid3
+node registerUser.js $path'2' $ca3_url 'user2' $mspid3
+echo 
+echo "################################"
+echo "TRANSACT TO ORG3"
+echo "################################"
+echo 
+node invoke.js $path'2' 'mychannel3' $peer4 $orderer 'user2' 'mycc' 'invoke' $args4 $event_peer
+echo 
+echo "################################"
+echo "QUERY ORG3"
+echo "################################"
+echo 
+node query.js $path'2' 'mychannel3' $peer4 'user2' 'mycc' 'query' $args1
 
 
 
