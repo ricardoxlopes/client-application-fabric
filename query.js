fcba79bfbe1fa9079a5f0f4e14d05b17618132c0
Ricardo Lopes <ricardoxlopes@hotmail.com>
2018-04-11 11:37:13 +0100

    files modified for loopback call

--- query.js
+++ query.js
@@ -1,84 +1,71 @@
-'use strict';
-/*
-* Copyright IBM Corp All Rights Reserved
-*
-* SPDX-License-Identifier: Apache-2.0
-*/
-/*
- * Chaincode query
- */
-
 var Fabric_Client = require('fabric-client');
 var path = require('path');
 var util = require('util');
 var os = require('os');
 
-var argPath = process.argv.slice(2)[0];
-var argChannel = process.argv.slice(2)[1];
-var argPeer = process.argv.slice(2)[2];
-var argUser = process.argv.slice(2)[3];
-var argChaincodeId = process.argv.slice(2)[4];
-var argFcn = process.argv.slice(2)[5];
-var argQueryArguments = process.argv.slice(2)[6];
-
 var fabric_client = new Fabric_Client();
-
-// setup the fabric network
-var channel = fabric_client.newChannel(argChannel);
-var peer = fabric_client.newPeer(argPeer);
-
-channel.addPeer(peer);
-
-var member_user = null;
-var store_path = path.join(__dirname, argPath);
-//console.log('Store path:'+store_path);
-var tx_id = null;
-
-// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
-var res = Fabric_Client.newDefaultKeyValueStore({ path: store_path
-}).then((state_store) => {
-	// assign the store to the fabric client
-	fabric_client.setStateStore(state_store);
-	var crypto_suite = Fabric_Client.newCryptoSuite();
-	// use the same location for the state store (where the users' certificate are kept)
-	// and the crypto store (where the users' keys are kept)
-	var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
-	crypto_suite.setCryptoKeyStore(crypto_store);
-	fabric_client.setCryptoSuite(crypto_suite);
-
-	// get the enrolled user from persistence, this user will sign all requests
-	return fabric_client.getUserContext(argUser, true);
-}).then((user_from_store) => {
-	if (user_from_store && user_from_store.isEnrolled()) {
-		// console.log('Successfully loaded user1 from persistence');
-		member_user = user_from_store;
-	} else {
-		throw new Error('Failed to get user.... run registerUser.js');
-	}
 	
-	const request = {
-		//targets : --- letting this default to the peers assigned to the channel
-		chaincodeId: argChaincodeId,
-		fcn: argFcn,
-		args: JSON.parse(argQueryArguments)
-	};
+var queryLedger = (argPath,argChannel,argPeer,argUser,argChaincodeId,argFcn,argQueryArguments) => {
 
-	// send the query proposal to the peer
-	return channel.queryByChaincode(request);
-}).then((query_responses) => {
-	//console.log("Query has completed, checking results");
-	// query_responses could have more than one  results if there multiple peers were used as targets
-	if (query_responses && query_responses.length == 1) {
-		if (query_responses[0] instanceof Error) {
-			console.error("error from query = ", query_responses[0]);
+	// setup the fabric network
+	var channel = fabric_client.newChannel(argChannel);
+	var peer = fabric_client.newPeer(argPeer);
+
+	channel.addPeer(peer);
+
+	var member_user = null;
+	var store_path = path.join(__dirname, argPath);
+
+	// create the key value store
+	var res = Fabric_Client.newDefaultKeyValueStore({ path: store_path
+	}).then((state_store) => {
+		// assign the store to the fabric client
+		fabric_client.setStateStore(state_store);
+		var crypto_suite = Fabric_Client.newCryptoSuite();
+		// use the same location for the state store (where the users' certificate are kept)
+		// and the crypto store (where the users' keys are kept)
+		var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+		crypto_suite.setCryptoKeyStore(crypto_store);
+		fabric_client.setCryptoSuite(crypto_suite);
+
+		// get the enrolled user from persistence, this user will sign all requests
+		return fabric_client.getUserContext(argUser, true);
+	}).then((user_from_store) => {
+		if (user_from_store && user_from_store.isEnrolled()) {
+			console.log('Successfully loaded'+argUser+'from persistence');
+			member_user = user_from_store;
 		} else {
-			console.log(query_responses[0].toString());
-			//return query_responses[0].toString();
+			throw new Error('Failed to get user.... run registerUser.js');
 		}
-	} else {
-		console.log("No payloads were returned from query");
-	}
-}).catch((err) => {
-	console.error('Failed to query successfully :: ' + err);
-});
+		
+		const request = {
+			//targets : --- letting this default to the peers assigned to the channel
+			chaincodeId: argChaincodeId,
+			fcn: argFcn,
+			args: JSON.parse(argQueryArguments)
+		};
 
+		// send the query proposal to the peer
+		//if (argFcn === "query")
+		
+		return channel.queryByChaincode(request);
+		//else return fabric_client.queryChannels(peer,true);
+
+	}).then((query_responses) => {
+		console.log("Query has completed, checking results");
+		// query_responses could have more than one  results if there multiple peers were used as targets
+		if (query_responses && query_responses.length == 1) {
+			if (query_responses[0] instanceof Error) {
+				console.error("error from query = ", query_responses[0]);
+			} else {
+				console.log(query_responses[0].toString())
+			}
+		} else {
+			console.log("No payloads were returned from query");
+		}
+	}).catch((err) => {
+		console.error('Failed to query successfully :: ' + err);
+	});
+}
+
+module.exports.queryLedger = queryLedger
